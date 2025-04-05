a8d58ad6a502f24cf3e526f019171b69bee71f8a
lucky966 <lukirobert87@gmail.com>
2025-04-05 15:24:16 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,42 @@
+const { nanoid } = require("nanoid")
+const books = require("./books")
+
+const addBookHandler = (request, h) => {
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload
+    const id = nanoid(16)
+    const insertedAt = new Date().toISOString()
+    const updatedAt = insertedAt
+    const newBook = {id,name, year, author, summary, publisher, pageCount, readPage, reading, insertedAt, updatedAt}
+
+    books.push(newBook)
+
+    const isSuccess = books.filter((book) => book.id === id).length > 0
+
+    if (isSuccess) {
+        const response = h.response({
+            status:"success",
+            message: "Buku berhasil ditambahkan",
+            data: {
+                bookId: id
+            }
+        })
+        response.code(201)
+        return response
+
+    }
+    const response = h.response({
+        status: "fail",
+        message: "Gagal menambahkan buku. Mohon di isi nama buku",
+    })
+    response.code(500)
+    return response
+}
+
+const getAllBooksHandler = () => ({
+    status:"success",
+    data:{
+        books
+    }
+})
+
+module.exports = {addBookHandler, getAllBooksHandler}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const { getAllBooksHandler, addBookHandler } = require("./handler")
+
+const routes =[
+    {
+        method:"GET",
+        path:"/books",
+        handler:getAllBooksHandler
+    },
+    {
+        method:"POST",
+        path:"/books",
+        handler:addBookHandler
+    }
+]
+
+
+module.exports = routes
